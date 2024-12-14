3d805c85699af160240e5eedd4a9c70620f78cc4
Verflog <pnbaranova@gmail.com>
2024-12-14 20:39:30 +0300

    Lab1

---
+++ main.py
@@ -0,0 +1,12 @@
+#homework
+#ex 1
+x = int(input("Введите число:"))
+for i in range(1, x + 1):
+    print(i)
+#ex 2
+a = int(input("Введите первое число:"))
+b = int(input("Введите второе число:"))
+if a > b:
+    print("Большее число:", a)
+else:
+    print("Большее число:", b)
